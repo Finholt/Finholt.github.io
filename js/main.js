4fd1de4eef51e2bc10e3d4d8e6ed4b7ef6f94a0b
Finholt <connor.finholt@gmail.com>
2018-09-30 20:34:55 -0500

    Re-doing home page to use new hex design

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 $(function () {
     $("#portfolioNav").hide();
     $("#aboutNav").hide();
-    $("#portfolioCard").hide();
-    $("#aboutCard").hide();
 
-    setTimeout(function(){
-        $("#portfolioCard").show();
-        $('#portfolioCard').addClass('animated fadeInUp');
-    }, 100);
-    setTimeout(function(){
-        $("#aboutCard").show();
-        $('#aboutCard').addClass('animated fadeInUp');
-    }, 200);
-    setTimeout(function(){
-        $("body").css("overflow", "auto");
-    }, 700);
+    // setTimeout(function(){
+    //     $("#portfolioCard").show();
+    //     $('#portfolioCard').addClass('animated fadeInUp');
+    // }, 100);
+    // setTimeout(function(){
+    //     $("#aboutCard").show();
+    //     $('#aboutCard').addClass('animated fadeInUp');
+    // }, 200);
+    // setTimeout(function(){
+    //     $("body").css("overflow", "auto");
+    // }, 700);
  });
+
+ function hexHover(hexId, hexPosition) {
+    var hex = document.getElementById(hexId);
+
+    if(!hex.classList.contains("activeHex_" + hexPosition)) {
+        setTimeout(function() {
+            hex.classList.add("activeHex_" + hexPosition);
+        }, 100);
+        setTimeout(function() {
+            hex.classList.remove("activeHex_" + hexPosition);
+        }, 1000);
+    }
+}
